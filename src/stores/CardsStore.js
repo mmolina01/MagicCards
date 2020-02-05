5446b77ed9fd71e90d198ebb7d25fe8ae6068421
Mauricio <mauriciomolina01@gmail.com>
2020-02-05 12:10:56 +0100

    Validation for no more cards in filter

--- src/stores/CardsStore.js
+++ src/stores/CardsStore.js
@@ -1,10 +1,10 @@
 import {EventEmitter} from 'events';
 import dispatcher from '../Dispatcher';
 
+const pageSize = 30;
 
 class CardsStore extends EventEmitter {
 	page = 1;
-	pageSize = 30;
 	filters = {};
 	loading = false;
 	loadingMore = false;
@@ -66,12 +66,15 @@ class CardsStore extends EventEmitter {
 		filterString += (this.filters && this.filters.name ? '&name='+this.filters.name : '');
 
 		return fetch('https://api.magicthegathering.io/v1/cards?page=' + this.page +
-		'&pageSize=' + this.pageSize +'&contains=imageUrl' + filterString)
+		'&pageSize=' + pageSize +'&contains=imageUrl' + filterString)
 		.then((response) => response.json())
 		.then((responseJson) => {
 
 			//API isn't 100% accurate with "contains=imageUrl", this filter removes cards without images
-			const cards = responseJson.cards.filter((card) => card.imageUrl);
+			//*Filtering messes up with calculating the number of cards per page, which is used to not making more request..
+				//..when there are no more cards to get (happens with filters). For not having to keep track of the total..
+				//..of cards after filtering it will stay off.
+			const cards = responseJson.cards;//.filter((card) => card.imageUrl);
 
 			return cards;
 		})
@@ -86,7 +89,7 @@ class CardsStore extends EventEmitter {
 	*/
 	async getNextPage() {
 
-		if (!this.loadingMore) {
+		if (!this.loadingMore && this.cards.length >= (pageSize * this.page)) {
 			this.loadingMore = true;
 			this.emitLoadingMoreState();
 	
